07682b55494af47daf338425f9b11bee2a0b2829
Ilkhom Sidikov <ilkhomzhon.sidikov@gmail.com>
2024-04-25 19:13:23 +0600

    Update launch-binary.sh

--- launch-binary.sh
+++ launch-binary.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 set -e
+set -x
 
-# Repository details
+#REPO="ionet-official/io_launch_binaries"
 REPO="ilkhom19/Credit_Card_Valitator"
 INSTALL_DIR="/usr/local/bin"
-BINARY_NAME="your_binary_name"
+BINARY_NAME="io-net-launcher"
 BINARY_PATH="$INSTALL_DIR/$BINARY_NAME"
 
 # Function to get the local binary version
 get_local_version() {
   if [ -f "$BINARY_PATH" ]; then
+    # Use the binary with the --version flag, parse its output to extract just the version number
     version_output="$($BINARY_PATH --version)"
+    # Assuming the version string is always at the end, following a space
     echo "$version_output" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+$'
   else
     echo "none"
@@ -26,40 +29,35 @@ get_latest_version() {
 # Function to download the latest binary
 download_binary() {
   case "$(uname -sm)" in
-    "Darwin arm64") FILENAME="$BINARY_NAME-macOS-arm64" ;;
-    "Linux x86_64") FILENAME="$BINARY_NAME-linux-amd64" ;;
+    "Darwin arm64") FILENAME="io-net-launcher-macOS-arm64" ;;
+    "Linux x86_64") FILENAME="io-net-launcher-linux-amd64" ;;
     *) echo "Unsupported architecture: $(uname -sm)" >&2; exit 1 ;;
   esac
 
   URL="https://github.com/$REPO/releases/download/$1/$FILENAME"
   echo "Downloading $FILENAME version $1 from GitHub releases"
-  curl -sSLf "$URL" -o "$BINARY_PATH" || {
+  if ! curl -sSLf "$URL" -o "$BINARY_PATH"; then
     echo "Failed to download or write to $INSTALL_DIR; try with sudo" >&2
     exit 1
-  }
+  fi
 
-  chmod +x "$BINARY_PATH" || {
+  if ! chmod +x "$BINARY_PATH"; then
     echo "Failed to set executable permission on $BINARY_PATH" >&2
     exit 1
-  }
+  fi
 
   echo "$BINARY_NAME version $1 is successfully installed"
 }
 
-# Check existing version and update if not the latest
 local_version=$(get_local_version)
 latest_version=$(get_latest_version)
 
 if [ "$local_version" != "$latest_version" ]; then
   echo "Local version ($local_version) is different from the latest version ($latest_version). Updating..."
-  if [ -f "$BINARY_PATH" ]; then
-    echo "Existing binary found. Deleting..."
-    rm "$BINARY_PATH" || {
-      echo "Failed to remove existing binary; please check your permissions or try manually" >&2
-      exit 1
-    }
-  fi
   download_binary "$latest_version"
 else
-  echo "No update needed. Local version ($local_version) is up-to-date."
+  echo
 fi
+
+# Execute the binary with the provided flags and options
+"$BINARY_PATH" "$@"
